674d15d2b93e1754f28ae46a34133ca0450bae56
Bakary <Bakary.Sylla@gmail.com>
2023-05-06 22:40:15 -0400

    Updated for selectable rolling window size

--- utils/sharpe_ratio.py
+++ utils/sharpe_ratio.py
@@ -7,12 +7,12 @@ from.data_prep import prep_data
 def rolling_sharpe(y):
     return np.sqrt(126) * (y.mean() / y.std()) # 21 days per month X 6 months = 126
 
-def sharpe_ratio (cryptocoin):
+def sharpe_ratio (cryptocoin, window_size):
     # Get the coin closing data form all the exchanges
     df = prep_data(cryptocoin)
     
     #df_daily_returns = df.pct_change().dropna()
     df_daily_returns = df.pct_change()
      
-    df1 = df_daily_returns.rolling(window=180).apply(rolling_sharpe)
+    df1 = df_daily_returns.rolling(window=window_size).apply(rolling_sharpe)
     return df1
