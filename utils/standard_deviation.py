9c554103f0cc4fb4ed00d5e2dde63af7b93eed57
AdrianJones313 <92553956+AdrianJones313@users.noreply.github.com>
2023-05-09 18:38:33 -0400

    Update standard_deviation.py

--- utils/standard_deviation.py
+++ utils/standard_deviation.py
@@ -7,17 +7,12 @@ from.data_prep import prep_data
 def rolling_sharpe(y):
     return np.sqrt(126) * (y.mean() / y.std()) # 21 days per month X 6 months = 126
 
-def standard_deviation (window_size):
-    # Read the aapl data, set the `date` as the index
-    aapl_csv_path = Path("data/aapl.csv")
-
-    aapl_df = pd.read_csv(
-        aapl_csv_path, index_col="date", infer_datetime_format=True, parse_dates=True
-    )
-    # Use the `drop` function to drop the extra columns
-    aapl_df.drop(columns=["volume"], inplace=True)
-    daily_returns_aapl = aapl_df.pct_change().dropna()
-    #window_size =7
-    #df1 = aapl_df.rolling(window=window_size).mean() 
-    df1 = daily_returns_aapl.rolling(window=window_size).apply(rolling_sharpe)
+def standard_deviation (cryptocoin, window_size):
+# Get the coin closing data form all the exchanges
+    df = prep_data(cryptocoin)
+    
+#df_daily_returns = df.pct_change().dropna()
+    df_daily_returns = df.pct_change()
+     
+    df1 = df_daily_returns.rolling(window=window_size).std()
     return df1
