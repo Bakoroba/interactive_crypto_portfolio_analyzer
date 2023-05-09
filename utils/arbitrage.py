a290a194752bcaf074c4c17bf4a3f19fc6ceb8ed
Marcus <legaremarcus@icloud.com>
2023-05-08 21:46:43 -0400

    First save checkpoint for the arbitrage code.

--- utils/arbitrage.py
+++ utils/arbitrage.py
@@ -2,12 +2,17 @@ import pandas as pd
 from pathlib import Path
 
 from.data_prep import prep_data
+
 def arbitrage(cryptocoin, window_size):
+    # Create a dataframe containing the crypto data
+   
     df = prep_data(cryptocoin)
-    #df = df.dropna()
-    df_daily_returns = df.pct_change(periods=int(window_size))
-    #df_daily_returns = df.rolling(windows=int(window_size)).mean()
+    
+    # Plot the price movements for the crypto currencies on the exchanges
+
+
+
      
      
-    df1 = df_daily_returns
+    
     return df
