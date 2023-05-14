618be1804292621c3c3fedfa00f9422d1a5db649
Bakoroba <Bakary.Sylla@gmail.com>
2023-05-13 21:15:48 -0400

    Update arbitrage.py

--- utils/arbitrage.py
+++ utils/arbitrage.py
@@ -1,18 +1,11 @@
+# Compare the closing prices of a crypto currency on all exchanges
+# Return the dataframe and max min values
 import pandas as pd
 from pathlib import Path
 
 from.data_prep import prep_data
-<<<<<<< HEAD
+
 def arbitrage(cryptocoin, window_size,value4):
     df = prep_data(cryptocoin)
     
     return df, df.max(), df.min()
-=======
-
-def arbitrage(cryptocoin, window_size):
-    # Create a dataframe containing the crypto data
-   
-    df = prep_data(cryptocoin)
-    
-    return df
->>>>>>> ef023ae487edf1336b375b087f78ef918debc2a9
